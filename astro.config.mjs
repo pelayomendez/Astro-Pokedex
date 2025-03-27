0e962975bee3898551066a70147be0365e61c14b
Pelayo Méndez XE95213 <pelayo.mendez.contractor@bbva.com>
2025-03-27 11:41:15 +0100

    Simplified example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
-
-import vercel from '@astrojs/vercel';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-  env: {
-    schema: {
-      GOOGLE_API_KEY: envField.string({ context: 'server', access: 'secret' })
-    }
-  },
+  output: 'server'
 });
